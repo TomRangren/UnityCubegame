077b90a07457e2074bd8902ad6eddbc7dc33f2bc
Tom <Tora02001@utb.vaxjo.se>
2020-03-24 18:21:28 +0100

    Fick det äntligen att fungera...

---
+++ Cubegame/Cubegame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cubegame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
